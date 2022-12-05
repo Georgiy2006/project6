af275278c0f5216875383de42543b59e337ae580
gosha7 <goshashonin@gmail.com>
2022-12-05 14:58:32 +0300

    java doc

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -3,8 +3,19 @@ package app;
 import io.github.humbleui.jwm.*;
 import java.util.function.Consumer;
 
+/**
+ * Класс окна приложения
+ */
+
 public class Application implements Consumer<Event> {
+    /**
+     * окно приложения
+     */
     private final Window window;
+
+    /**
+     * Конструктор окна приложения
+     */
     public Application() {
 
         window = App.makeWindow();
@@ -13,6 +24,12 @@ public class Application implements Consumer<Event> {
 
         window.setVisible(true);
     }
+
+    /**
+     * Обработчик событий
+     *
+     * @param e событие
+     */
     @Override
     public void accept(Event e) {
         if (e instanceof EventWindowClose) {
